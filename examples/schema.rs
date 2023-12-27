831fae64cb0feee17d772e41526164af697639b8
Shogo Hyodo <mmoshg8u@gmail.com>
2023-12-27 20:01:34 +0900

    Update

--- examples/schema.rs
+++ examples/schema.rs
@@ -1,7 +1,7 @@
 use cosmwasm_schema::write_api;
 use cosmwasm_std::Empty;
 
-use cw721_base::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use {{project-name}}::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
